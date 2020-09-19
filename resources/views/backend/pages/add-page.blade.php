b9b92a6986f2c04977bebf7e024039f7f665e5db
Faiz <faizdaffa17@gmail.com>
2020-09-19 08:27:01 +0700

    Change add-pages and add-post

--- resources/views/backend/pages/add-page.blade.php
+++ resources/views/backend/pages/add-page.blade.php
@@ -61,7 +61,7 @@
 	</div>
 </div>
 
-<script src="{{url('public/ckeditor/ckeditor.js')}}"></script>
+<script src="{{url('ckeditor/ckeditor.js')}}"></script>
 <script>
 	CKEDITOR.replace('description', { "filebrowserBrowseUrl": "ckfinder\/ckfinder.html", "filebrowserImageBrowseUrl": "ckfinder\/ckfinder.html?type=Images", "filebrowserFlashBrowseUrl": "ckfinder\/ckfinder.html?type=Flash", "filebrowserUploadUrl": "ckfinder\/core\/connector\/php\/connector.php?command=QuickUpload&type=Files", "filebrowserImageUploadUrl": "ckfinder\/core\/connector\/php\/connector.php?command=QuickUpload&type=Images", "filebrowserFlashUploadUrl": "ckfinder\/core\/connector\/php\/connector.php?command=QuickUpload&type=Flash" });	
 </script>
